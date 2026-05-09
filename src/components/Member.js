630f437b176049eb9d0d6887293af372e0526ebf
Kirk Alyn Santos <kirkalyn13@gmail.com>
2026-05-09 05:10:08 -0400

    feat: members data

--- src/components/Member.js
+++ src/components/Member.js
@@ -1,6 +1,11 @@
-const Member = () => {
+import Text from "./Text"
+
+const Member = ({ title, content, imageSrc }) => {
   return (
-    <div>Member</div>
+    <div className="member">
+        <img src={imageSrc} className="img-members" width="200" height="200" position="sticky" alt=""/> 
+        <Text title={title} content={content} />
+    </div>
   )
 }
 
